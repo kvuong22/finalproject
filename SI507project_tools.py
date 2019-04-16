2b12712f557f27397bf6ef2c43631e58c2c346d7
Kim Vuong <kvuong@umich.edu>
2019-04-16 15:38:30 -0400

    added models for db

--- SI507project_tools.py
+++ SI507project_tools.py
@@ -25,8 +25,48 @@ body_content = main_soup.find('div',{'class':'page_section__body'})
 
 list_of_cities = body_content.find_all('h3')
 
-cities_lst = []
+# cities_lst = []
+data_lst = []
+for city in list_of_cities:
+    data_lst.append(city.text)
 for c in list_of_cities:
-    cities_lst.append(c.text)
+    info_content = c.find_all('section',{'class':'spot_list'})
+    for section in info_content:
+        each_section_lst = []
 
+    # cities_lst.append(city.text)
 # print(cities_lst)
+
+# info_content = main_soup.find('section',{'class':'spot_list'})
+#
+# locations = info_content.find_all('h1 class="spot_list__spot__name"')
+#
+# location_info_lst = []
+# for c in locations:
+#     location_info_lst.append(c.string)
+
+
+
+
+
+
+#     all_info.append(spot_name.text)
+#
+# print(all_info)
+
+#models for database
+class Cities(Base):
+    __tablename__ = 'cities'
+    City_ID = Column(Integer, primary_key=True, autoincrement=True)
+    City_Name = Column(String(250))
+    Admin = Column(String(250))
+    Population = Column(Integer)
+
+
+class Locations(Base):
+    __tablename__ = "locations"
+    Location_ID = Column(Integer, primary_key=True, autoincrement=True)
+    Location_Name
+    City = Column(Integer, ForeignKey('cities.City_ID'))
+    Bloom_Date = Column(String(150))
+    cities = relationship('Cities')
