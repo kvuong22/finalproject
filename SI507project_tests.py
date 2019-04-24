0dbc0f842592b7992f207a7231cf23ef61f42962
Kim Vuong <kvuong@umich.edu>
2019-04-24 00:52:22 -0400

    updated tests. wrote unittest for scraping function.

--- SI507project_tests.py
+++ SI507project_tests.py
@@ -1,19 +1,40 @@
-import sqlite3
+# import sqlite3
 import unittest
+from SI507project_tools import *
+
+#tests for data scraped from website
+class TestDataScrape(unittest.TestCase):
+    def test_scraping_site(self):
+        results = get_bloom_site_data(START_URL)
+        results_lst = ['Shinjuku Gyoen', 'Early April']
+        self.assertTrue(results[0],results_lst)
+
+    def test_list_length(self):
+        results = get_bloom_site_data(START_URL)
+        self.assertEqual(len(results),48)
+
+    def test_type(self):
+        results = get_bloom_site_data(START_URL)
+        self.assertEqual(list,type(results))
+
 
 
 #tests for database
-class ProjectSQLiteDBTests(unittest.TestCase):
-
-    def setUp(self):
-        self.conn = sqlite3.connect("jpbloom.sqlite")
-        self.cur = self.conn.cursor()
 
 
-    def test_for_countries_table(self):
-		self.cur.execute("select City_Name, Admin, Population from cities where city name = 'Tokyo'")
-		data = self.cur.fetchone()
-		self.assertEqual(data,('Tokyo', 'Tokyo', 35676000), "Testing data that results from selecting city Tokyo")
+
+
+
+
+    # def setUp(self):
+    #     self.conn = sqlite3.connect("jpbloom.sqlite")
+    #     self.cur = self.conn.cursor()
+    #
+    #
+    # def test_for_countries_table(self):
+	# 	self.cur.execute("select City_Name, Admin, Population from cities where city name = 'Tokyo'")
+	# 	data = self.cur.fetchone()
+	# 	self.assertEqual(data,('Tokyo', 'Tokyo', 35676000), "Testing data that results from selecting city Tokyo")
 #example from HW5 test file
 	# def test_chocolate_insert_works(self):
 	# 	chocolate = ('A. Morin', 'Kappi', '2015', 70.0, "Haiti", 2.75)
@@ -46,10 +67,10 @@ class ProjectSQLiteDBTests(unittest.TestCase):
 	# 	self.assertTrue(data, "Testing that result of selecting based on relationship between chocolatebars and countries does work")
 	# 	self.assertTrue(len(data) in [1795, 1796], "Testing that there is in fact the amount of data entered that there should have been -- based on this query of everything in both tables.{}".format(len(data)))
 
-
-	def tearDown(self):
-		self.conn.commit()
-		self.conn.close()
+    #
+	# def tearDown(self):
+	# 	self.conn.commit()
+	# 	self.conn.close()
 
 
 if __name__ == '__main__':
